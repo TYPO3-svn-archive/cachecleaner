7830ee93852e455bd12c8e35596aafdc0582f157
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-09-04 08:25:48 +0000

    Initial import of kickstarter code
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cachecleaner/trunk@23989 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "cachecleaner"
+#
+# Auto generated 04-09-2009 10:21
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Cache Cleaner',
+	'description' => 'Automation tool for clearing up old entries in cache tables',
+	'category' => 'misc',
+	'author' => 'Francois Suter (Cobweb)',
+	'author_email' => 'typo3@cobweb.ch',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"424b";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:19:"doc/wizard_form.dat";s:4:"2503";s:20:"doc/wizard_form.html";s:4:"62af";}',
+);
+
+?>
