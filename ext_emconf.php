fa19eee4f25a7b00c47585c9657cbeee4cc5596b
francois <francois@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-11-09 12:57:48 +0000

    Released to TER as version 1.0.2
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/cachecleaner/trunk@26310 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "cachecleaner"
 #
-# Auto generated 06-11-2009 21:31
+# Auto generated 09-11-2009 13:57
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.1',
+	'version' => '1.0.2',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"7842";s:10:"README.txt";s:4:"03b5";s:34:"class.tx_cachecleaner_lowlevel.php";s:4:"a3fb";s:24:"configuration_sample.php";s:4:"b620";s:16:"ext_autoload.php";s:4:"e9e3";s:21:"ext_conf_template.txt";s:4:"ef02";s:12:"ext_icon.gif";s:4:"c17e";s:17:"ext_localconf.php";s:4:"a50b";s:13:"locallang.xml";s:4:"8401";s:14:"doc/manual.sxw";s:4:"943b";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"7371";s:10:"README.txt";s:4:"03b5";s:34:"class.tx_cachecleaner_lowlevel.php";s:4:"102b";s:24:"configuration_sample.php";s:4:"b620";s:16:"ext_autoload.php";s:4:"e9e3";s:21:"ext_conf_template.txt";s:4:"ef02";s:12:"ext_icon.gif";s:4:"c17e";s:17:"ext_localconf.php";s:4:"a50b";s:13:"locallang.xml";s:4:"583b";s:14:"doc/manual.sxw";s:4:"86de";}',
 	'suggests' => array(
 	),
 );
